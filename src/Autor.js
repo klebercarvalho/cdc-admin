5b4a793cabfe729b265ff0c2e81e3a751aa36888
Kleber Carvalho <klebercarvalho@MacIBM-Kleber.local>
2020-01-16 09:54:34 -0300

    Stable version using higher order components

--- src/Autor.js
+++ src/Autor.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
 
+/*
+ *
+ */
 class FormularioAutor extends Component {
 
     constructor() {
