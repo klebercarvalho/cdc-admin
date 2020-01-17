7e70dd32126c6d48d607e7156c7e73793d2118b2
Kleber Carvalho <klebercarvalho@MacIBM-Kleber.local>
2020-01-16 21:19:31 -0300

    react router

--- src/TratadorErros.js
+++ src/TratadorErros.js
@@ -1,15 +1,10 @@
 import PubSub from 'pubsub-js';
 
 export default class TratadorErros {
-    publicaErros(erros) {
-        console.log('erros');
-        console.log(erros);
-        for (let i = 0; i < erros.errors.length; i++) {
-            const erro =erros.errors[i];
-            PubSub.publish('erro-validacao', erro);
-            
-        }
-
-    }
-
+	publicaErros(erros){
+		for(var i=0;i<erros.errors.length;i++){
+			var erro = erros.errors[i];
+			PubSub.publish("erro-validacao",erro);
+		}
+	}
 }
